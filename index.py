cf30b430d43bbbb35ce2f98f43a00354fc112e1a
Csongorka2013 <czakocsongor@turr.hu>
2023-12-08 11:17:09 +0100

    függvény folytatás

--- index.py
+++ index.py
@@ -8,5 +8,11 @@ def prim():
         return False
     if szam==2:
         return True
+    for i in range((szam/2)+1):
+        if szam%i==0:
+            return False
+        else:
+            return True
+
 
     
