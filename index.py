38cf68851be3bd99959a44c8055f1bf00cea58bb
Csongorka2013 <czakocsongor@turr.hu>
2023-12-08 11:15:35 +0100

    függvény

--- index.py
+++ index.py
@@ -1,2 +1,12 @@
 print("Ezzel a programmal prímszámot számolhatsz és ellenőrizhetsz.\n")
 szam=int(input("Írj be egy számot: "))
+
+
+
+def prim():
+    if szam%2==0:
+        return False
+    if szam==2:
+        return True
+
+    
