f394e88f2008fa65a73f81bb898507a478840581
Csongorka2013 <czakocsongor@turr.hu>
2023-12-08 11:24:13 +0100

    függvény változtatás

--- index.py
+++ index.py
@@ -3,19 +3,12 @@ szam=int(input("Írj be egy számot: "))
 
 
 
-def prim():
-    if szam%2==0:
-        return False
-    if szam==2:
-        return True
-    for i in range(szam):
-        if szam%i==0:
-            return False
-            
-    return True
+prim=True
+if szam%2==0:
+    prim=False
 
-if prim==True:
-    print("Ez prim.")
-else:
-    print("Ez nem prim.")
-    
+for i in range(szam):
+    if szam%i==0:
+        prim=False
+    else:
+        prim=True
